be659315a5acd5e4f89959d4d71f324441c3b3dd
Garmaz23 <garmaz.ivan@gmail.com>
2022-06-15 17:27:12 +0200

    first

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <section id="about">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <img src="tapweb\public\assets\about1.png" alt="About" className="w-75 mt-5"/>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/components/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <section id="home">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-md-8 mt-5">
+                            <h1 className="diplay-4 fw-border mb-4 text-center text-white"> Feels the Fresh Busiines Perspectiove</h1>
+                            <p className="lead text-center fs-4 mb-5 text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                            Natus reprehenderit amet corporis voluptatibus autem ipsum aperiam pariatur quas, 
+                            deleniti magnam libero molestiae omnis laboriosam nemo quod, 
+                            hic exercitationem non dolores?</p>
+                            <div className="buttons">
+                                <div className="btn btn-light me-4 rounded-pill px-4 py-2">Get Quote</div>
+                                <div className="btn btn-outline-light me-4 rounded-pill px-4 py-2">Roadmap</div>
+
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+
+        </div>
+    );
+}
+
+export default Home;
