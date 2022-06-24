c600296787043a03e39883c856c9ea57c46c8024
Garmaz23 <garmaz.ivan@gmail.com>
2022-06-25 01:49:53 +0200

    Added msg shema and API

--- server/app.js
+++ server/app.js
@@ -17,6 +17,7 @@ const port = process.env.PORT;
 
 //Require Model
 const Users = require("./models/userShema");
+const Message = require("./models/msgShema");
 
 //This method is used to get data and cookies from frontend
 app.use(express.json());
@@ -28,7 +29,7 @@ app.get("/", (req, res) => {
 });
 
 //Reqistration
-app.post('/register', async (req, res) => {
+app.post("/register", async (req, res) => {
   try {
     //Get body or data
     const username = req.body.username;
@@ -50,38 +51,62 @@ app.post('/register', async (req, res) => {
     res.status(400).send(error);
   }
 });
-//Login 
-app.post('/login', async (req, res)=>{
+
+//Login
+app.post("/login", async (req, res) => {
   try {
-      const email = req.body.email;
-      const password = req.body.password;
+    const email = req.body.email;
+    const password = req.body.password;
 
-      // Find user if exist
-      const user = await Users.findOne({email : email});
-      if(user){
-          // Verify password
-          const isMatch = await bcryptjs.compare(password, user.password);
+    // Find user if exist
+    const user = await Users.findOne({ email: email });
+    if (user) {
+      // Verify password
+      const isMatch = await bcryptjs.compare(password, user.password);
 
-          if(isMatch){
-              // Generate token which is define in user schema
-              const token = await user.generateToken();
-              res.cookie("jwt", token, {
-                  // Expires token in 24 h
-                  expires : new Date(Date.now() + 86400000),
-                  httpOnly : true
-              })
-              res.status(200).send("LoggedIn")
-          }else{
-              res.status(400).send("Invalid Credentials");
-          }
-      }else{
-          res.status(400).send("Invalid Credentials");
+      if (isMatch) {
+        // Generate token which is define in user schema
+        const token = await user.generateToken();
+        res.cookie("jwt", token, {
+          // Expires token in 24 h
+          expires: new Date(Date.now() + 86400000),
+          httpOnly: true,
+        });
+        res.status(200).send("LoggedIn");
+      } else {
+        res.status(400).send("Invalid Credentials");
       }
-
+    } else {
+      res.status(400).send("Invalid Credentials");
+    }
   } catch (error) {
-      res.status(400).send(error);
+    res.status(400).send(error);
   }
-})
+});
+
+//Message
+app.post("/message", async (req, res) => {
+  try {
+    //Get body or data
+    const name = req.body.name;
+    const email = req.body.email;
+    const message = req.body.message;
+
+    const sendMsg = new Message({
+      name: name,
+      email: email,
+      message: message,
+    });
+    //save method is used to create user or insert user
+    //but before savig or inserting, pass will hash
+    //after hash, it till save to db
+    const created = await sendMsg.save();
+    console.log(created);
+    res.status(200).send("Message sent");
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 //Run server
 
